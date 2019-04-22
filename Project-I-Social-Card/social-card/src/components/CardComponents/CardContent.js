853dc436f713c3e23461659e31030ae09bce0900
cashcoder88 <cashglobe@gmail.com>
2019-04-22 16:58:01 -0600

    Finished MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,7 +5,7 @@ import './Card.css';
 
 const CardContent = () => {
 
-    return(
+    return (
       <div className = "card-content">
         <h4>Get started with React</h4>
         <p>React makes it painless to create interactive UIs. Design simple views for each state in your
