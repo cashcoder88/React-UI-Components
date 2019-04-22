c2a709cbbae855d44420a48eeeec04a109fbf617
cashcoder88 <cashglobe@gmail.com>
2019-04-22 15:12:33 -0600

    First commit

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './components/CardComponents/CardBanner.js';
+import CardContent from './components/CardComponents/CardContent.js';
+
+
+
+function CardContainer() {
+  return (
+      <div className="card-container">
+            <CardBanner />
+            <CardContent />
+         </div>
+      );
+}
+// https://www.reactjs.org
+export default CardContainer;
+
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+//        <h2>React makes it painless to create interactive UIs. Design simple views for each state in your application.</h2>
+//        <a href="reactjs.org">reactjs.org</a>
+
+function CardContent() {
+    return (
+        <h1>Get started with React</h1>
+    );
+}
+
+export default CardContent;
