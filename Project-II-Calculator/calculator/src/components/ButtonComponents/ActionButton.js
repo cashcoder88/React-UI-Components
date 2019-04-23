a14c9489d664e550a2b6eae158c5fcd37385a0b9
cashcoder88 <cashglobe@gmail.com>
2019-04-23 15:39:27 -0600

    Finished MVP, sorry forgot to do more commits

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Button.css';
+
+import NumberButton from './NumberButton'
+function ActionButton() {
+    return (
+        <div>
+            <div className="action-main">
+                <div className="action-zero">
+                    <p>0</p>
+                </div>
+            </div> 
+            <div className="final">
+                <NumberButton textProp="clear" buttonStyle="btnBig"/>
+                <NumberButton textProp="÷" buttonStyle="btnR"/>
+            </div>
+        </div>
+    )
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,37 @@
 import React from 'react';
 import './Display.css';
+
+import NumberButton from '../ButtonComponents/NumberButton'
+import ActionButton from '../ButtonComponents/ActionButton'
+
+function CalculatorDisplay() {
+    return (
+        <div className="calc-main">
+            <ActionButton />
+            <div className="first">
+                <NumberButton textProp="7" buttonStyle="btnW"/>
+                <NumberButton textProp="8" buttonStyle="btnW"/>
+                <NumberButton textProp="9" buttonStyle="btnW"/>
+                <NumberButton textProp="x" buttonStyle="btnR"/>
+            </div>
+            <div className="second">
+                <NumberButton textProp="4" buttonStyle="btnW"/>
+                <NumberButton textProp="5" buttonStyle="btnW"/>
+                <NumberButton textProp="6" buttonStyle="btnW"/>
+                <NumberButton textProp="-" buttonStyle="btnR"/>
+            </div>
+            <div className="third">
+                <NumberButton textProp="1" buttonStyle="btnW"/>
+                <NumberButton textProp="2" buttonStyle="btnW"/>
+                <NumberButton textProp="3" buttonStyle="btnW"/>
+                <NumberButton textProp="+" buttonStyle="btnR"/>
+            </div>
+            <div className="final">
+                <NumberButton textProp="0" buttonStyle="btnBig"/>
+                <NumberButton textProp="=" buttonStyle="btnR"/>
+            </div>
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
